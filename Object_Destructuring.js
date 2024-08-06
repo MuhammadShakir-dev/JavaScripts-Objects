68475f0bc30f2d480ef92229f852001b6adc1852
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-08-06 23:34:47 +0500

    Objects Desctructure

---
+++ Object_Destructuring.js
@@ -0,0 +1,13 @@
+const courseDetails = {
+    courseName: "Wed Development bootcamp",
+    courseInstructor: "Muhammad Shakir",
+    courseOfferCompany: "Bytewise Limited",
+    courseDuration: "10 hrs",
+    courseProfeciencyLevel: "Begineer - Advanced"
+}
+
+const { courseName: name, courseInstructor: instructor, courseOfferCompany : company, courseDuration : duration } = courseDetails;
+console.log(name);
+console.log(instructor);
+console.log(company);
+console.log(duration);
