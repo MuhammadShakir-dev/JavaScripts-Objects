0302742f0bbb3096c25f31947987bc351503563e
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-08-09 16:16:18 +0500

    some IIFE discussion

---
+++ Functions-JS/IIFE.js
@@ -0,0 +1,11 @@
+// Imediately Invoked Function Expressions IIFE.
+
+// this is named IIFE
+(function DBCNTONE() {
+    console.log("Database one connected");
+})();
+
+((name) => { 
+    console.log(`Server ${name}, is created `);
+})("Google")
+
