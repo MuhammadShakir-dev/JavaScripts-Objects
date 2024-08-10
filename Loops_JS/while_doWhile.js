ff9b66c5f9c18d19eb62c14bfe0d5cf512b14152
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-08-10 13:06:09 +0500

    while & do while loop

---
+++ Loops_JS/while_doWhile.js
@@ -0,0 +1,23 @@
+// while loop
+
+let num = 10;
+while (num >= 1) { 
+    // console.log(`value is ${num}`);
+    num--;
+}
+
+// use array
+let ary = ["Batman", "Superman", "Spiderman"];
+let i = 0;
+while (i < ary.length) { 
+    // console.log(`value is ${ary[i]}`);
+    i++;
+}
+
+// do while loops
+
+let score = 10;
+do {
+    console.log(`Value is score = ${score}`);
+    score--
+} while (score >= 1);
