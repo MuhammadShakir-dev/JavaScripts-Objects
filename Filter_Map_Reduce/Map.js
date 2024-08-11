a1b8ba26a8a50a52f1a1d4a03f6f934ff6ef854b
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-08-11 22:55:13 +0500

    map array method

--- Filter_Map_Reduce/Map.js
+++ Filter_Map_Reduce/Map.js
@@ -57,3 +57,12 @@ function generateRandomNums(arySeven) {
     })
 }
 
+// methods chaining
+let ary = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let updatedAry = ary
+    .map((nums) => nums * 10)
+    .map((nums) => nums + 1)
+    .filter((nums) => nums >= 40)
+
+console.log(updatedAry);
+                
